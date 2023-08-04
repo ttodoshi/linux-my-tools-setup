b24bcc4b4e6570acb623d77cb79ae4fab0cac2d5
todoshi <ttodoshi@users.noreply.github.com>
2023-08-04 22:58:37 +0300

    nerd font download

--- download_apps.sh
+++ download_apps.sh
@@ -16,4 +16,5 @@ sudo snap install chromium
 sudo add-apt-repository multiverse && sudo apt install steam
 sudo snap install gimp
 # nvim (AstroNvim)
+mkdir -p ~/.local/share/fonts && cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
 nvim_type=linux64.deb$ && wget -O '/home/todoshi/Downloads/nvim.deb' $(curl -s https://api.github.com/repos/neovim/neovim/releases/latest | jq -r ".assets[] | select(.name | test(\"${nvim_type}\")) | .browser_download_url") && sudo dpkg -i ~/Downloads/nvim.deb && rm ~/Downloads/nvim.deb && mv ~/.config/nvim ~/.config/nvimbackup && git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim && nvim +PackerSync
